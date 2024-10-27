e8a19f24792ecdc6c7b7d0c44354c0e1bc95aeab
Eduardo <eduardomell06@hotmail.com>
2024-10-27 16:10:16 -0300

    [refactor]: adjust product crud

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,20 +1,6 @@
 class ProductsController < ApplicationController
+  before_action :fetch_product, only: %i[ show edit update destroy ]
   def index
-    # query = "
-    #         SELECT *
-    #         FROM product
-    #         ;
-    # "
-    # @result = ActiveRecord::Base.connection.execute(
-    #   ApplicationRecord.sanitize_sql_array(
-    #     [ query,  opt: nil ]
-    #   )
-    # )
-    # @result = @result.map { |item| Product.new(item) }
-    # @products = @result
-
-    # @products = Product.all
-    
     @products = Product.all
   end
 
@@ -24,30 +10,22 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
-    if @product.save
-      return redirect_to @product
-    end
+    return redirect_to @product if @product.save
+
     render :new
   end
 
-  def show
-    @product = Product.find(params[:id])
-  end
+  def show;end
 
-  def edit
-    @product = Product.find(params[:id])
-  end
+  def edit;end
 
   def update
-    @product = Product.find(params[:id])
-    if @product.update(product_params)
-      return redirect_to @product
-    end
+    return redirect_to @product if @product.update(product_params)
+
     render :edit
   end
 
   def destroy
-    @product = Product.find(params[:id])
     @product.destroy
     redirect_to products_path
   end
@@ -59,6 +37,6 @@ class ProductsController < ApplicationController
   end
 
   def fetch_product
+    @product = Product.find(params[:id])
   end
-
 end
